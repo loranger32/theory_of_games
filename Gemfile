776b32004999a444e86a54ced6123056ea914912
Laurent G <laurentguinotte@gmail.com>
2019-02-11 14:11:59 +0100

    Reimplement gemfile + restructure game launching code

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
 source 'https://rubygems.org'
 
-gem 'colorize'
-gem 'pry'
-gem 'rake'
+gem 'bundler', '~> 2.0', '>= 2.0.1'
+gem 'rake', '~> 12.3', '>= 12.3.2'
+gem 'colorize', '~> 0.8.1'
 
+group :development do
+  gem 'minitest', '~> 5.11', '>= 5.11.3'
+  gem 'minitest-reporters', '~> 1.3', '>= 1.3.6'
+  gem 'pry', '~> 0.12.2'
+end
--- lib/game_theory.rb
+++ lib/game_theory.rb
@@ -1,15 +1,19 @@
 #!/usr/bin/env ruby
 
-require 'pry'
-require 'colorize'
+require 'rubygems'
+require 'bundler'
+require 'bundler/setup'
+Bundler.require(:default, :development)
 
 require_relative 'game_theory/game_loop_class'
-require_relative 'game_theory/game_engine_class'
+require_relative 'game_theory/game_turn_engine_class'
 require_relative 'game_theory/player_factory_class'
 require_relative 'game_theory/decision_maker_class'
 require_relative 'game_theory/reporter_class'
 
+players = PlayerFactory.new.generate_players
+game_turn_engine = GameTurnEngine.new(players)
 
-GameLoop.new(GameTurnEngine.new(PlayerFactory.new.generate_players)).run
+GameLoop.new(game_turn_engine).run
 
 at_exit { puts "\nMerci d'avoir joué.".yellow }
