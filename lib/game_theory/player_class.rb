6f55155a51665162ed4a72dc8d55136b967b2315
Laurent G <laurentguinotte@gmail.com>
2019-02-11 12:50:27 +0100

    Implement the player class

--- lib/game_theory/player_class.rb
+++ lib/game_theory/player_class.rb
@@ -1,2 +1,59 @@
 class Player
+
+  MAX_GAIN = 5
+  MEDIUM_GAIN = 3
+  NO_GAIN = 0
+
+  def initialize(name: 'random', player_type: :random)
+    @name = name
+    @player_type = player_type
+    @move = nil
+    @score = 0
+  end
+
+  def play_move
+    @move = case @player_type
+            when :cooperator then :cooperates
+            when :traitor then :betrays
+            when :random then choose_random_move
+            else
+              :do_not_know
+            end
+  end
+
+  def cooperates?
+    @move == :cooperates
+  end
+
+  def betrays?
+    @move == :betrays
+  end
+
+  def earn_max
+    @score += MAX_GAIN
+  end
+
+  def earn_medium
+    @score += MEDIUM_GAIN
+  end
+
+  def loose
+    @score += NO_GAIN
+  end
+
+  def choose_random_move
+    rand(0..1) == 0 ? :betrays : :cooperates
+  end
 end
+
+
+good_guy = Player.new(name: 'Good guy', player_type: :cooperator)
+bad_guy = Player.new(name: 'Bad guy', player_type: :traitor)
+
+good_guy.play_move
+bad_guy.play_move
+
+p good_guy.cooperates?
+p bad_guy.betrays?
+
+
