cadb5e17998c52e32c704f2862303a144e36f141
Laurent G <laurentguinotte@gmail.com>
2019-02-12 09:22:26 +0100

    Fix an issue in turn_engine_test - still WIP, but passing

--- spec/turn_engine_test.rb
+++ spec/turn_engine_test.rb
@@ -1,17 +1,14 @@
 require_relative 'spec_helpers'
-require_relative '../lib/game_theory/game_turn_engine_class'
-
-# WIP
+require_relative '../lib/game_theory/turn_engine_class'
 
 class GameTurnEngineInitialisationTest < Minitest::Test
   def setup
     @player1 = Minitest::Mock.new
-    
     @player2 = Minitest::Mock.new
-    @player2.expect(:cooperates?, true)
+    @game_logic = Minitest::Mock.new
   end
 
   def test_it_take_one_array_as_unique_argument_test
-    assert_silent { GameTurnEngine.new([@player1, @player2]) }
+    assert_silent { TurnEngine.new([@player1, @player2], @game_logic) }
   end
 end
