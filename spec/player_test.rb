0686174aa4bfa4b48336a094bdfbeb75acf4ee76
Laurent G <laurentguinotte@gmail.com>
2019-02-19 21:18:08 +0100

    Fix player_test

--- spec/player_test.rb
+++ spec/player_test.rb
@@ -3,7 +3,8 @@ require_relative '../lib/game_theory/player_class'
 
 class PlayerAttributesTest < Minitest::Test
   def setup
-    @player = Player.new(name: 'Test Player', behavior: :random)
+    @score = Minitest::Mock.new
+    @player = Player.new(@score, name: 'Test Player', behavior: :random)
   end
 
   def test_player_has_a_name
@@ -14,8 +15,14 @@ class PlayerAttributesTest < Minitest::Test
     assert_equal :random, @player.behavior
   end
 
-  def test_player_has_a_score_attribute_set_to_zero_by_default
-    assert_equal 0, @player.score
+  def test_player_has_a_score_attribute
+    assert_respond_to(@player, :score)
+  end
+
+  def test_player_score_method_sends_the_correct_message_to_the_score_object
+    @score.expect(:total, 0)
+    @player.score
+    @score.verify
   end
 
   def test_player_has_a_move_attribute_set_to_nil_by_default
@@ -25,9 +32,10 @@ end
 
 class PlayerMovesTest < Minitest::Test
   def setup
-    @good_guy = Player.new(name: 'Test Good Guy', behavior: :cooperator)
-    @bad_guy = Player.new(name: 'Test Bad Guy', behavior: :traitor)
-    @random_guy = Player.new(name: 'Test Random Guy', behavior: :random)
+    @score = Minitest::Mock.new
+    @good_guy = Player.new(@score, name: 'Test Good Guy', behavior: :cooperator)
+    @bad_guy = Player.new(@score, name: 'Test Bad Guy', behavior: :traitor)
+    @random_guy = Player.new(@score, name: 'Test Random Guy', behavior: :random)
     @players = [@good_guy, @bad_guy, @random_guy]
   end
 
@@ -48,53 +56,32 @@ class PlayerMovesTest < Minitest::Test
 end
 
 class PlayerScoreTest < Minitest::Test
-  # Copy Paste from lib/game_theory.rb, instead of loading it
-  
   def setup
-    @max_gain = MAX_GAIN = 5 
-    @medium_gain = MEDIUM_GAIN = 3
-    @min_gain = MIN_GAIN = 0
-    @player = Player.new(name: 'Test Player', behavior: :cooperator)
-  end
-
-  def check_score_is_zero
-    assert_equal 0, @player.score
+    @score = Minitest::Mock.new
+    @player = Player.new(@score, name: 'Test Player', behavior: :cooperator)
   end
 
   def test_player_can_earn_max
-    check_score_is_zero
+    @score.expect(:earn_max, nil)
     @player.earn_max
-    assert_equal @max_gain, @player.score
+    @score.verify
   end
 
   def test_player_can_earn_medium
-    check_score_is_zero
+    @score.expect(:earn_medium, nil)
     @player.earn_medium
-    assert_equal @medium_gain, @player.score
+    @score.verify
   end
 
   def test_player_can_earn_min
-    check_score_is_zero
+    @score.expect(:earn_min, nil)
     @player.earn_min
-    assert_equal @min_gain, @player.score
-  end
-
-  def test_player_score_keeps_track_of_all_results
-    total = @max_gain + @max_gain + @min_gain + @medium_gain + @medium_gain
-    2.times { @player.earn_max }
-    @player.earn_min
-    2.times { @player.earn_medium }
-
-    assert_equal total, @player.score
+    @score.verify
   end
 
   def test_player_score_can_be_reset_to_zero
-    check_score_is_zero
-    @player.earn_min
-    @player.earn_medium
-    @player.earn_max
-    assert @player.score > 0
+    @score.expect(:reset!, nil)
     @player.reset_score
-    check_score_is_zero
+    @score.verify
   end
 end
