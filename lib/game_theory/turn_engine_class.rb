4fe1413349ef1e021df78f8677436671420c540d
Laurent G <laurentguinotte@gmail.com>
2019-02-19 21:32:12 +0100

    Fix the turn_engine tests

--- lib/game_theory/turn_engine_class.rb
+++ lib/game_theory/turn_engine_class.rb
@@ -1,7 +1,6 @@
 class TurnEngine
   
-  attr_reader :game_logic
-  attr_accessor :players
+  attr_reader :game_logic, :players
 
   def initialize(game_logic)
     @players = nil
--- spec/turn_engine_test.rb
+++ spec/turn_engine_test.rb
@@ -1,21 +1,39 @@
 require_relative 'spec_helpers'
 require_relative '../lib/game_theory/turn_engine_class'
 
-class GameTurnEngineInitialisationTest < Minitest::Test
+class TurnEngineTest < Minitest::Test
   def setup
     @player1 = Minitest::Mock.new
     @player2 = Minitest::Mock.new
     @players = [@player1, @player2]
     @game_logic = Minitest::Mock.new
-    @turn_engine = TurnEngine.new([@player1, @player2], @game_logic)
+    @turn_engine = TurnEngine.new(@game_logic)
   end
 
   def test_it_has_players_and_game_logic_attribute_readers
-    assert_equal @players, @turn_engine.players
+    assert_respond_to(@turn_engine, :players)
+    assert_respond_to(@turn_engine, :game_logic)
+  end
+
+  def test_it_has_players_attribue_set_to_nil_on_initialisation
+    assert_nil @turn_engine.players
+  end
+
+  def test_it_has_a_game_logic_attribute_set_to_an_object_on_initialisation
     assert_equal @game_logic.object_id, @turn_engine.game_logic.object_id
   end
 
-  def test_play_turn_actually_play_the_whole_turn
+  def test_it_can_assigns_players_to_self_and_game_logic
+    @game_logic.expect(:assign_players, nil, [@players])
+    @turn_engine.assign_players(@players)
+    assert_equal @players, @turn_engine.players
+    @game_logic.verify
+  end
+
+  def test_play_turn_method_actually_play_the_whole_turn
+    @game_logic.expect(:assign_players, nil, [@players])
+    @turn_engine.assign_players(@players)
+    
     @players.each do |player|
       player.expect(:play_move, nil)
       player.expect(:reset_move, nil)
@@ -29,6 +47,9 @@ class GameTurnEngineInitialisationTest < Minitest::Test
   end
 
   def test_it_can_reset_player_scores
+    @game_logic.expect(:assign_players, nil, [@players])
+    @turn_engine.assign_players(@players)
+    
     @players.each { |player| player.expect(:reset_score, nil) }
     @turn_engine.reset_players_score
     @players.each(&:verify)
