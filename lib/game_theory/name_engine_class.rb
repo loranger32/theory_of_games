5c94281605bcd08f42a44685e35e84c94c76dc67
Laurent G <laurentguinotte@gmail.com>
2019-03-11 18:20:08 +0100

    remoive IO operations from the NameEngine class - tests must be adapted

--- lib/game_theory/name_engine_class.rb
+++ lib/game_theory/name_engine_class.rb
@@ -1,58 +1,63 @@
+# A custom error class for invalid name argument
+class InvalidNameArgumentError < ArgumentError; end
+
 # Class responsible for name choice, both custom and random
 class NameEngine
-  include Displayable
-  include Validable
-
   # Allow word characters, a new line or an empty string
   NAME_PATTERN = /\A[\w+]|\n|\z/.freeze
 
   def initialize(random_name_list)
-    Displayable.set_io_variables_on(self)
     @random_names = random_name_list
     @choosen_names = []
   end
 
-  def choose_player_name(player_number)
-    name = ask_player_choice(player_number)
-
-    # The comparison with nil is needed for the tests
-    if name == '' || name.nil?
-      pick_random_name
-    else
-      name = ensure_uniqueness_of(name)
-      store_and_return_name(name.chomp)
-    end
+  def create_name(choice = nil)
+    validate_argument(choice)
+    choice.nil? || choice.empty? ? pick_random_name : process_choice(choice)
   end
 
   def pick_random_name
-    random_name = random_names.sample
+    name = random_names.sample
+    name = random_names.sample while choosen_names.include?(name)
+    store_and_return_name(name.chomp)
+  end
 
-    random_name = random_names.sample while choosen_names.include?(random_name)
+  def process_choice(name)
+    if unique_name?(name) && valid_name?(name)
+      store_and_return_name(name.chomp)
+    elsif !unique_name?(name)
+      :existent_name
+    elsif !valid_name?(name)
+      :invalid_name
+    else
+      raise InvalidNameArgumentError, "Could not proceed choice of '#{name}'"
+    end
+  end
 
-    store_and_return_name(random_name)
+  def reset_names!
+    @choosen_names = []
   end
 
   private
 
   attr_reader :random_names, :choosen_names
 
-  def ask_player_choice(player_number)
-    question = "Choisissez un nom pour le joueur #{player_number}, ou 'entrée'\
- pour un nom par défaut:"
-    prompt(question)
-    obtain_a_valid_input_from(NAME_PATTERN)
+  def validate_argument(name)
+    err_msg = "Invalide choice argument. Expect nil or a string, got #{name}\
+ which is of class #{name.class}."
+    raise InvalidNameArgumentError, err_msg unless name.nil? || name.is_a?(String)
   end
 
-  def ensure_uniqueness_of(name)
-    while choosen_names.include?(name)
-      prompt("Le nom #{name} est déjà pris, choisissez en un nouveau")
-      name = input.gets
-    end
-    name
+  def unique_name?(name)
+    !choosen_names.include?(name)
   end
 
   def store_and_return_name(name)
     choosen_names << name
     name
+  end
+
+  def valid_name?(name)
+    name.match?(NAME_PATTERN)
   end
 end
--- lib/game_theory/player_factory_class.rb
+++ lib/game_theory/player_factory_class.rb
@@ -30,20 +30,39 @@ class PlayerFactory
   def ask_number_of_players_to_create
     prompt('Combien de joueurs voulez-vous (2 - 9) ?')
     pattern = /\A[2-9]\z/
-
     choice = obtain_a_valid_input_from(pattern)
     choice.to_i
   end
 
   def collect_data_for_player_creation(number_of_players)
     1.upto(number_of_players) do |player_number|
-      name = name_engine.choose_player_name(player_number)
-      behavior_choice = ask_behavior_to_player
-      behavior = behavior_factory.create_behavior(behavior_choice)
-      players << player_class.new(name: name, score_recorder: score_class.new,
-                                  behavior: behavior)
+      choice = ask_player_name_choice(player_number)
+      name = name_engine.create_name(choice)
+      while %I[existent_name invalid_name].include?(name)
+        if name == :existent_name
+          prompt 'Ce nom existe déjà, veuillez en choisir un autre'
+        elsif name == :invalid_name
+          prompt 'Ce nom est invalide, veuillez en choisir un autre'
+        end
+        
+        choice = retrieve_input
+        name = name_engine.create_name(choice)
+      end
+
+    behavior_choice = ask_behavior_to_player
+    behavior = behavior_factory.create_behavior(behavior_choice)
+    players << player_class.new(name: name, score_recorder: score_class.new,
+                                behavior: behavior)
     end
   end
+
+  def ask_player_name_choice(player_number)
+    question = "Choisissez un nom pour le joueur #{player_number}, ou 'entrée'\
+ pour un nom par défaut:"
+    prompt(question)
+    retrieve_input
+  end
+
 
   def confirm_players?
     clear_screen
@@ -56,6 +75,7 @@ class PlayerFactory
 
   def collect_data_again
     @players = []
+    @name_engine.reset_names!
     print_message('Ok, on recommence.')
   end
 
@@ -72,5 +92,4 @@ class PlayerFactory
     prompt(question)
     obtain_a_valid_input_from_list %w[t n h r l ]
   end
-
 end
