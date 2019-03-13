87bf312298d2090ce151ddbdf05a34fd4963d747
Laurent G <laurentguinotte@gmail.com>
2019-03-13 15:40:41 +0100

    Add the renamed files for Name Factory

---
+++ lib/game_theory/name_factory_class.rb
@@ -0,0 +1,64 @@
+# A custom error class for invalid name argument
+class InvalidNameArgumentError < ArgumentError; end
+
+# Class responsible for name choice, both custom and random
+class NameFactory
+  # Allow word characters, maximum of 3 sequences separated by space(s)
+  NAME_PATTERN = /\A\w+(\s+\w+){0,2}\z/.freeze
+
+  attr_reader :choosen_names
+
+  def initialize(random_name_list)
+    @random_names = random_name_list
+    @choosen_names = []
+  end
+
+  def create_name(choice = nil)
+    validate_argument(choice)
+    choice.nil? || choice.empty? ? pick_random_name : process_choice(choice)
+  end
+
+  def pick_random_name
+    name = random_names.shuffle.pop
+    store_and_return_name(name)
+  end
+
+  def process_choice(name)
+    if unique_name?(name) && valid_name?(name)
+      store_and_return_name(name.chomp)
+    elsif !unique_name?(name)
+      :existent_name
+    elsif !valid_name?(name)
+      :invalid_name
+    else
+      raise InvalidNameArgumentError, "Could not proceed choice of '#{name}'"
+    end
+  end
+
+  def reset_names!
+    @choosen_names = []
+  end
+
+  private
+
+  attr_reader :random_names
+
+  def validate_argument(name)
+    err_msg = "Invalide choice argument. Expect nil or a string, got #{name}\
+ which is of class #{name.class}."
+    raise InvalidNameArgumentError, err_msg unless name.nil? || name.is_a?(String)
+  end
+
+  def unique_name?(name)
+    !choosen_names.include?(name)
+  end
+
+  def store_and_return_name(name)
+    choosen_names << name
+    name
+  end
+
+  def valid_name?(name)
+    name.match?(NAME_PATTERN)
+  end
+end
---
+++ spec/name_factory_test.rb
@@ -0,0 +1,66 @@
+require_relative 'spec_helpers'
+require_relative '../lib/game_theory/name_factory_class'
+
+class NameFactoryTest < Minitest::Test
+  RANDOM_TEST_NAMES = %w[alex ann bert bob evan iris larry lolo mike moe].freeze
+
+  def setup
+    @name_factory = NameFactory.new(RANDOM_TEST_NAMES)
+  end
+
+  def test_it_responds_to_create_name_method
+    assert_respond_to(@name_factory, :create_name)
+  end
+
+  def test_it_raises_an_error_if_argument_is_not_nil_or_string
+    assert_raises(InvalidNameArgumentError) { @name_factory.create_name(:name) }
+    assert_raises(InvalidNameArgumentError) { @name_factory.create_name(['jo']) }
+    assert_raises(InvalidNameArgumentError) { @name_factory.create_name(1234) }
+  end
+
+  def test_it_creates_and_stores_a_random_unique_name_without_arguments
+    1.upto(5) do |counter|
+      result = @name_factory.create_name
+      assert_includes RANDOM_TEST_NAMES, result
+      assert_includes @name_factory.choosen_names, result
+      assert_equal counter, @name_factory.choosen_names.size
+    end
+  end
+
+  def test_it_creates_and_stores_a_random_unique_name_with_empty_string
+    1.upto(5) do |counter|
+      result = @name_factory.create_name('')
+      assert_includes RANDOM_TEST_NAMES, result
+      assert_includes @name_factory.choosen_names, result
+      assert_equal counter, @name_factory.choosen_names.size
+    end
+  end
+
+  def test_it_creates_and_stores_a_valid_unique_name_with_valid_name_argument
+    valid_names = %w[pierre paul jacques antoine richard]
+
+    valid_names.each_with_index do |name, index|
+      result = @name_factory.create_name(name)
+      assert_equal name, result
+      assert_includes @name_factory.choosen_names, name
+      assert_equal index + 1, @name_factory.choosen_names.size
+    end
+  end
+
+  def test_it_reports_the_choice_of_an_existent_name
+    @name_factory.create_name('roger')
+    assert_equal :existent_name, @name_factory.create_name('roger')
+  end
+
+  def test_it_reports_the_choice_of_an_invalid_name
+    assert_equal :invalid_name, @name_factory.create_name('$*£ù')
+  end
+
+  def test_it_can_reset_choosen_names
+    %w[riri fifi loulou].each { |name| @name_factory.create_name(name) }
+    assert_equal 3, @name_factory.choosen_names.size
+    @name_factory.reset_names!
+    assert_instance_of Array, @name_factory.choosen_names
+    assert_empty @name_factory.choosen_names
+  end
+end
